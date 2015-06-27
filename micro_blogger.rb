effb21385e7981a114789e8670c7a4ec90f8f020
Cody Hacker <jbalsamo@cox.net>
2015-06-27 16:15:17 -0700

    tweets and direct messages

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -7,4 +7,48 @@ class MicroBlogger
     puts "Initializing MicroBlogger"
     @client = JumpstartAuth.twitter
   end
+
+  def tweet(message)
+  	if message.length > 140
+  		puts "That tweet is too darned long!"
+  		return
+  	else
+  		@client.update(message)
+  		puts "This tweet should be successful!"
+  	end
+  end
+
+  def dm(target, message)
+    screen_name = @client.followers.collect { |follower| @client.user(follower).screen_name }
+    if screen_name.include?target
+      puts "Trying to send #{target} this direct message:"
+      puts message
+      message = "d @#{target} #{message}"
+      tweet(message)
+    else
+      puts "You can only direct message people who follow you"
+    end
+  end
+
+  def run
+  	puts "Welcome to the JSL Twitter Client!"
+  	command = ""
+  	while command != "q"
+  		printf "enter command: "
+      input = gets.chomp
+      parts = input.split(" ")
+      command = parts[0]
+      case command
+        when 'q' then puts "Goodbye!"
+        when 't' then tweet(parts[1..-1].join(" "))
+        when 'dm' then dm(parts[1], parts[2..-1].join(" "))
+        else
+          puts "Sorry, I don't know how to #{command}"
+      end
+  	end
+  end	
+
 end
+
+blogger = MicroBlogger.new
+blogger.run
