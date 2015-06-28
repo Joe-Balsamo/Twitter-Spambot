ea3c0c4de0e0921e24b749f17c68fe95b1b924ce
Cody Hacker <jbalsamo@cox.net>
2015-06-27 17:53:40 -0700

    tweet short URL

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -78,6 +78,7 @@ class MicroBlogger
         when 'spam' then spam_my_followers(parts[1..-1].join(" "))
         when 'elt' then everyones_last_tweet
         when 's' then puts shorten(parts[1..-1].join(" "))
+        when 'turl' then tweet(parts[1..-2].join(" ") + " " + shorten(parts[-1]))
         else
           puts "Sorry, I don't know how to #{command}"
       end
